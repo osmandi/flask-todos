47c93a95f803ed9878380dfae2ac0a35edc8f623
tito <tito@pop-os.localdomain>
2019-08-11 06:29:41 -0500

    Implementacion de Bootstrap y guardar sesiones de manera segura

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, make_response, redirect, render_template
-from flask-bootstrap import Bootstrap
+from flask import Flask, request, make_response, redirect, render_template, session
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SECRETO INSEGURO'
 
 
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video al productor']
@@ -24,14 +26,16 @@ def error_server():
 def index():
     user_ip = request.remote_addr
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip) # Save ip user on Cookie
+    #response.set_cookie('user_ip', user_ip) # Save ip user on Cookie
+    session['user_ip'] = user_ip
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    #user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
 
-#    return "Hello World Platzi, tu IP es {}".format(user_ip) 
+    #return "Hello World Platzi, tu IP es {}".format(user_ip) 
     context = {
             'user_ip': user_ip,
             'todos': todos,
